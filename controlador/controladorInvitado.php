5716c658fb5be9ae9d703b431a54a3cdb68fad1c
Miguel010293 <48523518+Miguel010293@users.noreply.github.com>
2020-04-07 11:49:27 -0500

    Validacion de invitado
    La validaciónpara confirmación del invitado en caso de eligir la opcion "no"

--- controlador/controladorInvitado.php
+++ controlador/controladorInvitado.php
@@ -9,43 +9,60 @@ include("../dao/DAOinvitacion.php");
 
 if(isset($_GET['agrega'])){
 
-  
-    $_SESSION['nombre']= $_POST['nombre'];
-    $i= new Invitacion();
-    $p = new Persona();
-    
-    $p->nombre=$_POST['nombre'];
-    $p->telefono=$_POST['telefono'];
-        
-    $insertPers = DAOinvitado::registraPersona($p);
-     
-    if($insertPers){
-    
+
+  $_SESSION['nombre'] = $_POST['nombre'];
+  $i = new Invitacion();
+  $p = new Persona();
+
+  $p->nombre = $_POST['nombre'];
+  $p->telefono = $_POST['telefono'];
+
+  $insertPers = DAOinvitado::registraPersona($p);
+
+  if ($insertPers) {
+
     $resul = DAOinvitado::idpersona($p);
-    
-        foreach($resul as $data){
-          $i->invitado =$data[0];
-        }
+
+    foreach ($resul as $data) {
+      $i->invitado = $data[0];
+    }
+
+    if (isset($_POST['idimagen'])) {
+      $i->obsequio = $_POST['idimagen'];
+    } else {
+      $i->obsequio = null;
+    }
+    $i->acompanantes = $_POST['Npersonas'];
+    if (isset($_POST['confirmacion'])) {
+      $i->confirmacion = $_POST['confirmacion'];
+    } else {
+      $i->confirmacion = null;
+    }
+    if (isset($_POST['idimagen'])) {
+      $i->obsequio = $_POST['idimagen'];
+    } else {
+      $i->obsequio = null;
+    }
+    if (isset($_POST['regopcional'])) {
+      $i->regopcional = $_POST['regopcional'];
+    } else {
+      $i->regopcional = null;
+    }
+
+    $insertInvi = DAOinvitado::registraInvitado($i);
+
+    if ($insertInvi) {
+
+      if ($_POST['confirmacion'] = 'si') {
+        header("location:../vista/succes.php");
+      } else {
+        header("location:../vista/rechazo.php");
+      }
+    } else {
+
+      header("location:../");
+    }
         
-        if(isset($_POST['idimagen'])){$i->obsequio=$_POST['idimagen'];}else{ $i->obsequio= null;}
-        
-        $i->acompanantes=$_POST['Npersonas'];
-        if(isset($_POST['confirmacion'])){$i->confirmacion=$_POST['confirmacion'];}else{$i->confirmacion=null;}
-        if(isset($_POST['idimagen'])){$i->obsequio=$_POST['idimagen'];}else{ $i->obsequio= null;}
-        if(isset($_POST['regopcional'])){$i->regopcional = $_POST['regopcional'];}else{$i->regopcional=null;}
-           
-        /*$insertInvi = DAOinvitado::registraInvitado($i);
-        if($_POST['cionfirmacion']='si'){
-
-        }else{
-
-        }*/
-        if($insertInvi){
-          header("location:../vista/succes.php");
-
-        }else{
-          echo "nel perro :V";
-        }
      
     
     }
--- vista/rechazo.php
+++ vista/rechazo.php
@@ -9,11 +9,21 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playball&family=Satisfy&display=swap" rel="stylesheet">
 </head>
 <body>
     
-<h1>UPS! parece que hubo un error. Porfavor intenta de nuevo o ponte en contacto con el organizador ;)</h1>
+<style>
+    #texto{
+
+font-family: 'Playball', cursive;
+}
+
+</style>
+<div id="texto">
+<h1>ASH que lastima que no aceptó gran hp </h1>
 <h2><?php  echo $_SESSION['nombre']?></h2>
+</div>
 <script type="text/javascript">
 
 setTimeout("location.href='FormInvitado.php'", 5000);
