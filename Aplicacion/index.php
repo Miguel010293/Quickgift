53d1580b497af82a2b5a8c6d2d7d7fb935514257
Anna Maria <48517924+anniep98@users.noreply.github.com>
2020-06-09 01:41:59 -0500

    Fronend change
    Se hizo algunos cambios del diseño del index.

--- Aplicacion/index.php
+++ Aplicacion/index.php
@@ -5,27 +5,30 @@
 
 <head>
     <meta charset="utf-8">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
     <!-- This file has been downloaded from Bootsnipp.com. Enjoy! -->
     <title>Index</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Source+Sans+Pro" rel="stylesheet">
     
     <style type="text/css">
         .profil {
             background: #008CBA;
             color: #008CBA;
         }
+        .profil .yacht-info__list li {
+    width:25%;
+}
 
         .img {
-            background-image: url("IMG/flore.png");
-            background-repeat: no-repeat;
+            background-image: url("IMG/living-room.jpg");
             background-size: cover;
         }
 
-        .profil .yacht-info__list li {
-            width: 25%;
-        }
 
         .olanaklar-kutu {
             background: #fff;
@@ -35,6 +38,7 @@
             float: left;
             width: 100%;
             margin: 15px 0px;
+            padding-top: 10px;
         }
 
         .olanaklar {
@@ -75,22 +79,48 @@
         }
 
         .letra {
-            font: oblique bold 120% cursive;
+            font-family: 'Source Sans Pro', sans-serif;
             color: black;
+            text-align:justify;
 
         }
 
         .button {
-            background-color: black;
+        
+            background-color: #008CBA;
             border: none;
             border-radius: 5px;
             color: white;
-            padding: 16px 32px;
+            padding: 15px 30px;
             text-align: center;
-            text-decoration: none;
             display: inline-block;
+            font-weight: 700;
+        }
+        .button:hover{
+            color: #fff;
+            color: rgba(255, 255, 255, 1) !important;
+            box-shadow: 0 5px 15px rgba(49, 138, 172, 1);
+            transition: all 0.1s ease;
+            text-decoration:none;
+        }
+        
+        .card-img-top{
+            padding-top: 45px;
+            padding-bottom: 20px;
+            height: 86%;
+        }
+
+        h1{
+            font-family: 'Kaushan Script', cursive;
+            font-size:3.5em;
+            letter-spacing:1px;
+            color: #008CBA;
+            margin-bottom:5px;
         }
         </style>
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        
 </head>
 
 <body>
@@ -100,8 +130,7 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="slider">
-                        <div class= " olanaklar-kutu
-                         ">
+                        <div class= "olanaklar-kutu">
                             <center>
                                 <h1>
                                     <p> HOME SHOWER<p>
@@ -109,21 +138,21 @@
                             </center>
                         </div>
 
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
                             <ol class="carousel-indicators">
                                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                             </ol>
-                            <div style="    border-radius: 0.25rem;" class="carousel-inner">
+                            <div style="border-radius: 0.25rem;" class="carousel-inner">
                                 <div class="carousel-item active">
                                     <img class="d-block w-100" style="height: 290px" src="IMG/invita.png" alt="First slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" style="height: 290px" src="IMG/flat2.png" alt="Second slide">
+                                    <img class="d-block w-100" style="height: 290px" src="IMG/home1.jpg" alt="Second slide">
                                 </div>
                                 <div class="carousel-item">
-                                    <img class="d-block w-100" style="height: 290px" src="IMG/flat3.png" alt="Third slide">
+                                    <img class="d-block w-100" style="height: 290px" src="IMG/flat2.png" alt="Third slide">
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -137,13 +166,14 @@
                         </div>
                     </div>
                     <div class="olanaklar-kutu">
+                        
 
                         <div class="olanaklar margin-top--22">
                             <h4 class="pb-3">!!HOLA FAMILIA Y AMIGOS¡¡</h4>
                             <p class="letra">¡Nos mudamos y lo celebramos! </p>
                             <p class="letra">Empezamos esta aventura y queremos que ustedes hagan parte de esta nueva etapa de nuestra vida, por este motivo los invitamos a nuestro Shower House el cual lo celebraremos el proximo 01 de Agosto a las 03:00 pm en Ciudad Montes. Contamos con tu asistencia.
                                 Tenemos un listado de algunas cosas, pero sabes!, se nos pueden pasar algunas otras, si por algún motivo quieres traer otra cosa que no se encuentra en el listado !No te preocupes! lo importante es contar contigo en este momento tan importante para nosotros.
-                                Las imágenes sirven como referencia.
+                                Las imágenes sirven como referencia.<br>
                                 Contamos contigo!!!<p>
 
                         </div>
@@ -151,9 +181,9 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card"></div>
-                    <img class="card-img-top" src="IMG/pareja.png" alt="Card image cap">
+                    <img class="card-img-top" src="IMG/coupleB.png" alt="Card image cap">
                     <div class="card-body text-center ">
-                    <a href="vista/FormInvitado.php" class="button button2 olanaklar letra">COMENCEMOS</a>
+                    <a href="vista/FormInvitado.php" class=" button olanaklar letra">COMENCEMOS</a>
 
                         
                     </div>
